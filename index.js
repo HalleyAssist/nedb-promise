814c7d01b2eac3b2b89765c9d908f9a6e91d3538
Mathew Heard <splitice@users.noreply.github.com>
2019-09-06 16:04:08 +1000

    support sorting on find

--- index.js
+++ index.js
@@ -1,44 +1,72 @@
-var NedbDatastore = require('nedb')
-var thenify = require('thenify')
+var NedbDatastore = require('nedb'),
+	thenify = require('thenify')
+
+class Finder {
+	constructor(cursor){
+		this._cursor = cursor
+		for(const k in cursor){
+			const fn = cursor[k]
+			if(typeof fn === 'function'){
+				this[k] = (...args)=>{
+					return new Finder(fn.call(curor, ...args))
+				}
+			}
+		}
+	}
+
+	then(...args){
+		return new Promise((resolve, reject)=>{
+			this._cursor.exec((err, docs)=>{
+				if(err) return reject(err)
+				resolve(docs)
+			})
+		}).then(...args)
+	}
+}
 
 function fromInstance(nedbInstance) {
 	var newDB = { nedb: nedbInstance }
 
-	var methods = [ 'loadDatabase', 'insert', 'find', 'findOne', 'count', 'update', 'remove', 'ensureIndex', 'removeIndex' ]
+	var methods = ['loadDatabase', 'insert', 'findOne', 'count', 'update', 'remove', 'ensureIndex', 'removeIndex']
 	for (var i = 0; i < methods.length; ++i) {
 		var m = methods[i]
 		newDB[m] = thenify(nedbInstance[m].bind(nedbInstance))
 	}
 
-	newDB.cfind = function(query, projections) {
+	newDB.find = function (...args) {
+		const result = nedbInstance.find(...args)
+		return new Finder(result)
+	}
+
+	newDB.cfind = function (query, projections) {
 		var cursor = nedbInstance.find(query, projections)
 		cursor.exec = thenify(cursor.exec.bind(cursor))
 		return cursor
 	}
 
-	newDB.cfindOne = function(query, projections) {
+	newDB.cfindOne = function (query, projections) {
 		var cursor = nedbInstance.findOne(query, projections)
 		cursor.exec = thenify(cursor.exec.bind(cursor))
 		return cursor
 	}
 
-	newDB.ccount = function(query) {
+	newDB.ccount = function (query) {
 		var cursor = nedbInstance.count(query)
 		cursor.exec = thenify(cursor.exec.bind(cursor))
 		return cursor
 	}
 
-  // added by bslee
-  newDB.setAutocompactionInterval = function(interval, minimumWritten) {
-    nedbInstance.persistence.setAutocompactionInterval(interval, minimumWritten)
-  }
+	// added by bslee
+	newDB.setAutocompactionInterval = function (interval, minimumWritten) {
+		nedbInstance.persistence.setAutocompactionInterval(interval, minimumWritten)
+	}
 
-  newDB.compact = function(cb) {
-    nedbInstance.persistence.compactDatafile()
-    if (typeof cb === 'function') {
-      nedbInstance.once('compaction.done', cb)
-    }
-  }
+	newDB.compact = function (cb) {
+		nedbInstance.persistence.compactDatafile()
+		if (typeof cb === 'function') {
+			nedbInstance.once('compaction.done', cb)
+		}
+	}
 
 	return newDB
 }
